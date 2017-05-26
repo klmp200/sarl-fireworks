2256128c81bf557a68e6b204992c2b0f684a4345
klmp200 <klmp200@klmp200.net>
2017-05-26 17:31:47 +0200

    Nice UI

--- src/main/generated-sources/sarl/io/sarl/demos/fireworks/Firework.java
+++ src/main/generated-sources/sarl/io/sarl/demos/fireworks/Firework.java
@@ -4,11 +4,13 @@ import io.janusproject.Boot;
 import io.janusproject.kernel.Kernel;
 import io.sarl.demos.fireworks.agents.LaunchingArea;
 import io.sarl.demos.fireworks.gui.FXMLViewerController;
+import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 
-@SarlSpecification("0.6")
+@SarlSpecification("0.5")
+@SarlElementType(8)
 @SuppressWarnings("all")
 public class Firework {
   public static Kernel main(final FXMLViewerController controller) {
@@ -16,6 +18,7 @@ public class Firework {
       Kernel _xblockexpression = null;
       {
         Boot.setOffline(true);
+        Boot.setVerboseLevel(7);
         _xblockexpression = Boot.startJanus(LaunchingArea.class, controller);
       }
       return _xblockexpression;
